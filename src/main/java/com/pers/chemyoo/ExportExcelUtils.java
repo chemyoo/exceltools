0b07a55958dd643e643f71ef7d247dab85d14ea9
chemyoo <chemyoo@Prd-liujianqing>
2021-05-19 15:23:43 +0800

    参数说明

--- src/main/java/com/pers/chemyoo/ExportExcelUtils.java
+++ src/main/java/com/pers/chemyoo/ExportExcelUtils.java
@@ -52,6 +52,21 @@ public class ExportExcelUtils
 		}
 	}
 
+//	XLSTransformer transformer = new XLSTransformer(); 
+//    File template = ResourceUtils.getFile("classpath:template/excel/claim_summary_report.xls");
+//    InputStream is = new FileInputStream(template); 
+//    Workbook workbook = transformer.transformMultipleSheetsList(is,results,monthNames, "results",new HashMap(),1); 
+//
+//
+//    public Workbook transformMultipleSheetsList(InputStream is, List objects, List newSheetNames, String beanName, Map beanParams, int startSheetNum) throws ParsePropertyException
+//    该方法里面的参数说明如下：
+//    1）is：即Template文件的一个输入流
+//    2）newSheetNames：即形成Excel文件的时候Sheet的Name
+//    3）objects：即我们传入的对应每个Sheet的一个Java对象，这里传入的List的元素为一个Map对象
+//    4）beanName：这个参数在jxls对我们传入的List进行解析的时候使用，而且，该参数还对应Template文件中的Tag，例如，beanName为map，那么在Template文件中取值的公式应该定义成${map.get("property1")}；如果beanName为payslip，公式应该定义成${payslip.get("property1")}
+//    5）beanParams：这个参数在使用的时候我的代码没有使用到，这个参数是在如果传入的objects还与其他的对象关联的时候使用的，该参数是一个HashMap类型的参数，如果不使用的话，直接传入new HashMap()即可
+//    6）startSheetNo：传入0即可，即SheetNo从0开始
+
 	public static void main(String[] args) throws IOException
 	{
 		InputStream is = FileUtils.openInputStream(new File("D:/2021.xlsx"));
